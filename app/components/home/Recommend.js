77a9bf09f1c9730f8a5fd501e023c184b3db79e3
ZhouYingKai1 <13625695226@163.com>
2017-09-15 18:08:13 +0800

    提交sectionList和下拉展示

--- app/components/home/Recommend.js
+++ app/components/home/Recommend.js
@@ -2,7 +2,7 @@
 个性推荐
 */
 import React from 'react'
-import { View, Image, Text, StyleSheet, ScrollView, InteractionManager, TouchableOpacity } from 'react-native'
+import { View, Image, Text, StyleSheet, SectionList, InteractionManager, TouchableOpacity } from 'react-native'
 import {Toast, Button } from 'antd-mobile';
 import px from '../../utils/pxToDp'
 import theme from '../../utils/theme'
@@ -10,68 +10,62 @@ import Swiper from 'react-native-swiper';
 
 const Recommend = (props)=> {
   // const { } = props
-  return(
-    <ScrollView style={{flex: 1}}>
-      <View stlye={styles.position}><Text style={{color: 'red', textAlign: 'center'}}>ddddd</Text></View>
-      <View style={{flex: 1}}>
-        <Swiper autoplay={true} style={{height: px(260)}} >
-          {
-            imgData.map((item, index)=> {
-              return(
-                <View key={index}>
-                  <Image source={{uri: item.img}} style={{height: px(260), width: theme.screenWidth}}/>
-                </View>  
-              )
-            }) 
-          }
-        </Swiper>
-        <View style={styles.iconList}>
-          <TouchableOpacity onPress={()=> console.log(2222)} style={styles.iconBox}>
-            <View style={styles.imgBorder}>
-              <Image source={require('../../img/icon_fm.png')} style={styles.icon}/>
-            </View>
-            <Text style={styles.iconText}>私人FM</Text>
-          </TouchableOpacity >
-          <TouchableOpacity onPress={()=> Toast.show(2)}  style={styles.iconBox}>
-            <View style={styles.imgBorder}>
-              <Image source={require('../../img/icon_fm.png')} style={styles.icon}/>
-            </View>
-            <Text style={styles.iconText}>每日推荐</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=> Toast.show(2)}  style={styles.iconBox}>
-            <View style={styles.imgBorder}>
-              <Image source={require('../../img/icon_cal.png')} style={[styles.icon,{justifyContent:'center',alignItems:'center'}]}>
-                <Text style={{marginTop: px(12), color: theme.themeColor, fontSize: px(20)}}>14</Text>
-              </Image>
-            </View>
-            <Text style={styles.iconText}>歌单</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=> Toast.show(3)}  style={styles.iconBox}>
-            <View style={styles.imgBorder}>
-              <Image source={require('../../img/icon_rank.png')} style={styles.icon}/>
-            </View>
-            <Text style={styles.iconText}>排行榜</Text>
-          </TouchableOpacity>
-        </View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
-        <View><Text>ddddd</Text></View>
+  const renderSec = (item)=> {
+    return(
+      <View  key={item.title}>
+        <Text>{item.title}</Text>
       </View>
-    </ScrollView>
+    )
+  }
+  const renderFirst = (item)=> {
+    return(
+      <View key={item.title}>
+        <Text style={{color: 'red'}}>{item.title}</Text>
+      </View>
+    )
+  }
+  const renderHeader = (item)=> {
+    console.log(item,'item')
+    return(
+      <Text>2222</Text>
+    )
+  }
+  return(
+    <View style={{flex: 1}}>
+      <Text style={{position: 'absolute', top: px(20), left: 0}}>根据你的喜好推荐首页内容</Text>
+      <SectionList
+        style={{position: 'absolute', top: px(20), left: 0, backgroundColor: '#eee'}}
+        sections={[
+          {
+            renderItem: renderHeader,
+            key: 's0',
+            data: imgData
+          },
+          {
+            renderItem: renderFirst,
+            key: 's1',
+            data: [{
+              title: '周莹凯',
+              title: '艾弗森',
+              title: '科比',
+              title: '加内特',
+            }]
+          },
+          {
+            renderItem: renderSec,
+            key: 's2',
+            data: [{
+              title: '牛魔王',
+              title: '至尊宝',
+              title: '紫霞',
+              title: '唐僧',
+            }]
+          },
+        ]}
+        ItemSeparatorComponent={() => <View style={{borderBottomColor: '#eee', borderBottomWidth: 1}}><Text></Text></View>}
+
+        />
+    </View>
   )
 }
 const styles = StyleSheet.create({
@@ -137,3 +131,48 @@ const imgData= [{
   img: 'http://ww4.sinaimg.cn/large/7a8aed7bjw1ezrtpmdv45j20u00spahy.jpg',
 }]
 export default Recommend
+
+// <ScrollView style={{flex: 1}}>
+//       <View stlye={styles.position}><Text style={{color: 'red', textAlign: 'center'}}>ddddd</Text></View>
+//       <View style={{flex: 1}}>
+//         <Swiper autoplay={true} style={{height: px(260)}} >
+//           {
+//             imgData.map((item, index)=> {
+//               return(
+//                 <View key={index}>
+//                   <Image source={{uri: item.img}} style={{height: px(260), width: theme.screenWidth}}/>
+//                 </View>  
+//               )
+//             }) 
+//           }
+//         </Swiper>
+//         <View style={styles.iconList}>
+//           <TouchableOpacity onPress={()=> console.log(2222)} style={styles.iconBox}>
+//             <View style={styles.imgBorder}>
+//               <Image source={require('../../img/icon_fm.png')} style={styles.icon}/>
+//             </View>
+//             <Text style={styles.iconText}>私人FM</Text>
+//           </TouchableOpacity >
+//           <TouchableOpacity onPress={()=> Toast.show(2)}  style={styles.iconBox}>
+//             <View style={styles.imgBorder}>
+//               <Image source={require('../../img/icon_fm.png')} style={styles.icon}/>
+//             </View>
+//             <Text style={styles.iconText}>每日推荐</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity onPress={()=> Toast.show(2)}  style={styles.iconBox}>
+//             <View style={styles.imgBorder}>
+//               <Image source={require('../../img/icon_cal.png')} style={[styles.icon,{justifyContent:'center',alignItems:'center'}]}>
+//                 <Text style={{marginTop: px(12), color: theme.themeColor, fontSize: px(20)}}>14</Text>
+//               </Image>
+//             </View>
+//             <Text style={styles.iconText}>歌单</Text>
+//           </TouchableOpacity>
+//           <TouchableOpacity onPress={()=> Toast.show(3)}  style={styles.iconBox}>
+//             <View style={styles.imgBorder}>
+//               <Image source={require('../../img/icon_rank.png')} style={styles.icon}/>
+//             </View>
+//             <Text style={styles.iconText}>排行榜</Text>
+//           </TouchableOpacity>
+//         </View>
+//       </View>
+//     </ScrollView>
